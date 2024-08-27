1387f0431446ab90c13d46870f7e51b18d5af0b9
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-08-26 22:15:06 -0400

    la prueba aprobo con 2008,2012 y 2016  es bisiesto

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,10 +1,25 @@
-function esBisiesto (anio)
+/*function esBisiesto (anio)
 {
     if (anio % 400 === 0)
     {
         return true;
     }
     return false;
+}*/
+
+
+
+function esBisiesto (anio)
+{
+    if (anio % 100 === 0 && anio % 4 ===0)
+    {
+        return false;
+    }
+    else if (anio % 4 === 0)
+    {
+        return true;
+    }
+    
 }
 
 export default esBisiesto;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -1,10 +1,10 @@
 import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
-    it("El 2000 deberia considerarse como año bisiesto", () => 
+    /*it("El 2000 deberia considerarse como año bisiesto", () => 
     {
         expect(esBisiesto(2000)).toEqual(true);
-    });
+    });*/
 
     it("El 1700 no deberia considerarse como año bisiesto", () => 
     {
@@ -22,5 +22,35 @@ describe("Es Bisiesto", () => {
     {
         expect(esBisiesto(2100)).toEqual(false);
     });
+
+    it("El 2008 deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(2008)).toEqual(true);
+    });
+    it("El 2012 deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(2012)).toEqual(true);
+    });
+    it("El 2016 deberia considerarse como año bisiesto", () => 
+    {
+         expect(esBisiesto(2016)).toEqual(true);
+    });
+/*
+    it("El 1700 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(1700)).toEqual(false);
+    });
+    it("El 1800 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(1800)).toEqual(false);
+    });
+    it("El 1900 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(1900)).toEqual(false);
+    });
+    it("El 2100 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(2100)).toEqual(false);
+    });*/
 });
 
