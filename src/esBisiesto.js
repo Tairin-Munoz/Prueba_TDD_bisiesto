99a96646e854d46b1a59079e089f7e7938affac4
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-08-26 21:45:36 -0400

    la prueba aprobo con 2000 es bisiesto

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,6 +1,10 @@
 function esBisiesto (anio)
 {
-  
+    if (anio % 400 === 0)
+    {
+        return true;
+    }
+    return false;
 }
 
 export default esBisiesto;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -6,10 +6,6 @@ describe("Es Bisiesto", () => {
     expect(esBisiesto(2000)).toEqual(true);
   });
 
-  it("El 5 no deberia considerarse como año bisiesto", () => 
-    {
-    expect(esBisiesto (5)).toEqual(false);
-  });
 
 });
 
