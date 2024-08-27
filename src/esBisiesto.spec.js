5d1b4d0c5c675485d684a84ca7fcf65c4fdc995b
Tairin Muñoz <tairinmunoz14@gmail.com>
2024-08-26 21:54:25 -0400

    la prueba aprobo con 1700,1800,1900 y 2100 no es bisiesto

--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -1,11 +1,26 @@
 import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
-  it("El 4 deberia considerarse como año bisiesto", () => 
-{
-    expect(esBisiesto(2000)).toEqual(true);
-  });
+    it("El 2000 deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(2000)).toEqual(true);
+    });
 
-
+    it("El 1700 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(1700)).toEqual(false);
+    });
+    it("El 1800 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(1800)).toEqual(false);
+    });
+    it("El 1900 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(1900)).toEqual(false);
+    });
+    it("El 2100 no deberia considerarse como año bisiesto", () => 
+    {
+        expect(esBisiesto(2100)).toEqual(false);
+    });
 });
 
